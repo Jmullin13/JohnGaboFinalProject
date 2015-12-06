c220be0cc3285daabde8f14660e14153c417494b
HouseStark <jmullin@smu.edu>
2015-12-06 16:44:31 -0600

    Initial Commit

---
+++ SearchEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SearchEngine
+//
+//  Created by John Mullin on 12/6/15.
+//  Copyright (c) 2015 Mullin Media. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
